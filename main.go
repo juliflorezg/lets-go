49c9e2bf611810f7daae16f40003248e0917e0fe
Julian Florez <forezjuli08@hotmail.com>
2024-01-08 14:32:31 -0500

    add - two new handlers for routes

--- main.go
+++ main.go
@@ -9,16 +9,24 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
-func main() { 
-  // Here we use the http.NewServeMux() fun to initialize a new servermux(router), then
-  // register the home function as the handler for the "/" URL route/pattern
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func main() {
+	// Here we use the http.NewServeMux() fun to initialize a new servermux(router), then
+	// register the home function as the handler for the "/" URL route/pattern
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+  // Here we're registering two new handler functions and corresponding URL patterns with the servermux
+	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/snippet/view", snippetView)
 
+	log.Println("starting server on port :4000")
 
-  log.Println("starting server on port :4000")
-
-
-  err:= http.ListenAndServe(":4000", mux)
-  log.Fatal(err)
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
