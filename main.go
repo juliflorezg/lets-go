cf98a074e5ea41654b6bcae9084ab6fa522e35d2
Julian Florez <forezjuli08@hotmail.com>
2024-01-08 14:21:48 -0500

    add - start a server, create a router(servemux) & create handler

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
-func main() {
-	fmt.Println("hello world!!")
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func main() { 
+  // Here we use the http.NewServeMux() fun to initialize a new servermux(router), then
+  // register the home function as the handler for the "/" URL route/pattern
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+
+  log.Println("starting server on port :4000")
+
+
+  err:= http.ListenAndServe(":4000", mux)
+  log.Fatal(err)
 }
