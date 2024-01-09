c6a1622bce5c4e0503d7fb80badfe3386b0b51a1
Julian Florez <forezjuli08@hotmail.com>
2024-01-09 12:47:04 -0500

    add - customizing response header based on method

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -17,6 +18,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println(r.Method)
+	if r.Method != "POST" {
+		// We use set to add an "Allow: POST" header to the response header map
+		w.Header().Set("allow", "POST") 
+		
+		w.WriteHeader(405)
+		// w.WriteHeader(404) // we can only use WriteHeader once per response and any subsequent try to change the status code once it has changed won't succeed, we get the error 2024/01/09 12:27:45 http: superfluous response.WriteHeader call from main.snippetCreate (main.go:25)
+		// w.Header().Set("allow", "POST") // ! won't work, must be called before any WriteHeader() or Write()
+		w.Write([]byte("Method not allowed")) 
+		
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 func snippetView(w http.ResponseWriter, r *http.Request) {
@@ -33,10 +48,10 @@ func main() {
 	mux.HandleFunc("/", home)
 	// Here we're registering two new handler functions and corresponding URL patterns with the servermux
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	// mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/view", snippetView)
 	//* by including a host name in the URL pattern, we can route requests based on the host part of the URL,
 	//* the handler would be invoked for requests like 'http://snippet.view.org/anypath'
-	mux.HandleFunc("snippet.view.org/", snippetView)
+	// mux.HandleFunc("snippet.view.org/", snippetView)
 	//subtree path, if we make a request to /foo it will automatically redirect to /foo/
 	mux.HandleFunc("/foo/", fooHandler)
 
