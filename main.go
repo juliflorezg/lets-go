2a512c331af3f2de31f313197605f48ee6aa0d25
Julian Florez <forezjuli08@hotmail.com>
2024-01-09 11:03:47 -0500

    add - extra info about handlers

--- main.go
+++ main.go
@@ -22,6 +22,9 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 func snippetView(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a specific snippet..."))
 }
+func fooHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Foo"))
+}
 
 func main() {
 	// Here we use the http.NewServeMux() fun to initialize a new servermux(router), then
@@ -30,10 +33,17 @@ func main() {
 	mux.HandleFunc("/", home)
 	// Here we're registering two new handler functions and corresponding URL patterns with the servermux
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	mux.HandleFunc("/snippet/view", snippetView)
+	// mux.HandleFunc("/snippet/view", snippetView)
+	//* by including a host name in the URL pattern, we can route requests based on the host part of the URL,
+	//* the handler would be invoked for requests like 'http://snippet.view.org/anypath'
+	mux.HandleFunc("snippet.view.org/", snippetView)
+	//subtree path, if we make a request to /foo it will automatically redirect to /foo/
+	mux.HandleFunc("/foo/", fooHandler)
 
 	log.Println("starting server on port :4000")
 
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+
+	// v:= bird object
 }
