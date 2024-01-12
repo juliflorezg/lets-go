7900032e16e8d824c97e3275e17931332f23193b
Julian Florez <forezjuli08@hotmail.com>
2024-01-12 12:04:01 -0500

    add - stack trace to serverError handler

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log/slog"
 	"net/http"
+	"runtime/debug"
 )
 
 // The serverError helper writes a log entry at Error level (including the request
@@ -12,23 +13,23 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 	var (
 		method = r.Method
 		uri    = r.URL.RequestURI()
+		trace  = string(debug.Stack())
 	)
 
-	app.logger.Error(err.Error(), slog.String("method", method), slog.String("uri", uri))
-  http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	app.logger.Error(err.Error(), slog.String("method", method), slog.String("uri", uri), "trace", trace)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
 // The clientError helper sends a specific status code and corresponding description
 // to the user. We'll use this to send responses like 400 "Bad
 // Request" when there's a problem with the request that the user sent
 func (app *application) clientError(w http.ResponseWriter, statusCode int) {
-  http.Error(w, http.StatusText(statusCode), statusCode)
+	http.Error(w, http.StatusText(statusCode), statusCode)
 }
 
 // For consistency, we'll also implement a notFound helper. This is simply a
 // convenience wrapper around clientError which sends a 404 Not Found response to
 // the user.
-func (app *application) notFound(w http.ResponseWriter){
-  app.clientError(w, http.StatusNotFound)
+func (app *application) notFound(w http.ResponseWriter) {
+	app.clientError(w, http.StatusNotFound)
 }
-
