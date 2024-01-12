a2e96220245ac8e2cce69bfb74f958c99750aa3d
Julian Florez <forezjuli08@hotmail.com>
2024-01-12 12:19:20 -0500

    add - use routes file & describe main function responsibilities

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,7 +12,9 @@ type application struct {
 }
 
 func main() {
+	//> this comment style represents the responsibilities of this main function
 	// new command line flag, name addr, default value :4000
+	//> pass the runtime configuration settings for the application
 	addr := flag.String("addr", ":4000", "HTTP Network Address")
 
 	// this assigns the value passed on runtime to the addr variable
@@ -23,32 +25,15 @@ func main() {
 		AddSource: true,
 	}))
 
+	//> Establish the dependencies for the handlers
 	app := &application{
 		logger: logger,
 	}
 
-	// Here we use the http.NewServeMux() fun to initialize a new servermux(router), then
-	// register the home function as the handler for the "/" URL route/pattern
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// we use the the created fileServer as a handler for any request coming to /static/
-	// for matching paths, we strip (remove) the '/static' from the path before it reaches the fileServer handler so if can give back the correct file.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	// Here we're registering two new handler functions and corresponding URL patterns with the servermuxx
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	//* by including a host name in the URL pattern, we can route requests based on the host part of the URL,
-	//* the handler would be invoked for requests like 'http://snippet.view.org/anypath'
-	// mux.HandleFunc("snippet.view.org/", snippetView)
-	//subtree path, if we make a request to /foo it will automatically redirect to /foo/
-	mux.HandleFunc("/foo/", app.fooHandler)
-
 	logger.Info("starting server", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, mux)
+	//> Run the HTTP server
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
