b29beb5aa03b770740463586fb169d740678eedf
Julian Florez <forezjuli08@hotmail.com>
2024-01-19 10:51:38 -0500

    add - cache for file templates (this way the files would only be parsed once)

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,10 +1,53 @@
 package main
 
-import "github.com/juliflorezg/lets-go/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/juliflorezg/lets-go/internal/models"
+)
 
 // Define a templateData type to act as the holding structure for
 // any dynamic data that we want to pass to our HTML templates.
 type templateData struct {
-	Snippet models.Snippet
-  Snippets []models.Snippet
+	Snippet  models.Snippet
+	Snippets []models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	// This map will act as the template cache
+	cache := map[string]*template.Template{}
+
+	// Use the filepath.Glob() function to get a slice of all filepaths that
+	// match the pattern "./ui/html/pages/*.tmpl". This will essentially gives
+	// us a slice of all the filepaths for our application 'page' templates
+	// like: [ui/html/pages/home.tmpl ui/html/pages/view.tmpl]
+	pages, err := filepath.Glob("./ui/html/pages*.tmpl.html")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		// Extract the file name (like 'home.tmpl') from the full filepath
+		// and assign it to the name variable.
+		name := filepath.Base(page)
+
+		// Create a slice containing the filepaths for our base template, any
+		// partials and the page.
+		files := []string{
+			"./ui/html/base.tmpl.html",
+			"./ui/html/partials/nav.tmpl.html",
+			page,
+		}
+
+		// Parse the files into a template set.
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts // keys would be like: home.tmpl.html 
+	}
+
+	return cache, nil
 }
