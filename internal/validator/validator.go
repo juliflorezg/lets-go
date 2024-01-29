a621bbb1b85e01f6e42e56302d6be7b0614dc04b
Julian Florez <forezjuli08@hotmail.com>
2024-01-29 16:50:59 -0500

    add - function descriptions

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -10,10 +10,13 @@ type Validator struct {
 	FieldErrors map[string]string
 }
 
+// Valid() returns true if the FieldErrors map doesn't contain any entries.
 func (v *Validator) Valid() bool {
 	return len(v.FieldErrors) == 0
 }
 
+// AddFieldError() adds an error message to the FieldErrors map (so long as no
+// entry already exists for the given key)
 func (v *Validator) AddFieldError(key, message string) {
 	if v.FieldErrors == nil {
 		v.FieldErrors = make(map[string]string)
@@ -32,14 +35,18 @@ func (v *Validator) CheckField(ok bool, key, message string) {
 	}
 }
 
+// NotBlank() returns true if a value is not an empty string
 func NotBlank(value string) bool {
 	return strings.TrimSpace(value) != ""
 }
 
+// MaxChars() returns true if a value contains no more than n characters.
 func MaxChars(value string, n int) bool {
 	return utf8.RuneCountInString(value) <= n
 }
 
+// PermittedValue() returns true if a value is in a list of specific permitted
+// values.
 func PermittedValue[T comparable](value T, permittedValues ...T) bool {
 	return slices.Contains(permittedValues, value)
 }
