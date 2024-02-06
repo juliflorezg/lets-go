3e74fa40c5f2f8061c49c5f124a0f0d981408413
Julian Florez <forezjuli08@hotmail.com>
2024-02-06 10:22:27 -0500

    add - new validator helpers for validating user input

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,6 +1,7 @@
 package validator
 
 import (
+	"regexp"
 	"slices"
 	"strings"
 	"unicode/utf8"
@@ -9,6 +10,8 @@ import (
 type Validator struct {
 	FieldErrors map[string]string
 }
+
+var EmailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 // Valid() returns true if the FieldErrors map doesn't contain any entries.
 func (v *Validator) Valid() bool {
@@ -49,4 +52,13 @@ func MaxChars(value string, n int) bool {
 // values.
 func PermittedValue[T comparable](value T, permittedValues ...T) bool {
 	return slices.Contains(permittedValues, value)
+}
+
+// MinChars() returns true if a value contains at least n characters.
+func MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
+}
+
+func Matches(value string, regex *regexp.Regexp) bool {
+	return regex.MatchString(value)
 }
