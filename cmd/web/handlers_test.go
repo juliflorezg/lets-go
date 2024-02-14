d087a0ee144cccf80b91b5a7a74ee57a5a4c8294
Julian Florez <forezjuli08@hotmail.com>
2024-02-14 15:07:54 -0500

    update - use of test helpers in e2e ping test

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"log/slog"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/juliflorezg/lets-go/internal/assert"
@@ -47,32 +43,13 @@ func TestPing(t *testing.T) {
 
 	// Create a new instance of our application struct. For now, this just
 	// contains a structured logger (which discards anything written to it)
-	app := application{
-		logger: slog.New(slog.NewTextHandler(io.Discard, nil)),
-	}
+	app := NewTestApplication(t)
 
-	// We then use the httptest.NewTLSServer() function to create a new test
-	// server, passing in the value returned by our app.routes() method as the
-	// handler for the server. This starts up a HTTPS server which listens on a
-	// randomly-chosen port of your local machine for the duration of the test.
-	// Notice that we defer a call to ts.Close() so that the server is shutdown
-	// when the test finishes
-	ts := httptest.NewTLSServer(app.routes())
+	ts := NewTestServer(t, app.routes())
 	defer ts.Close()
 
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
+	status, _, body := ts.get(t, "/ping")
 
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	body = bytes.TrimSpace(body)
-
-	assert.Equal(t, string(body), "OK")
+	assert.Equal(t, status, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
