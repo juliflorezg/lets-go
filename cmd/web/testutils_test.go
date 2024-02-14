63689def7749cb5bdb1a91faacaced8f6121df7d
Julian Florez <forezjuli08@hotmail.com>
2024-02-14 14:58:09 -0500

    add - helper functions for e2e tests

---
+++ cmd/web/testutils_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"io"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+// Create a newTestApplication helper which returns an instance of our
+// application struct containing mocked dependencies.
+func NewTestApplication(t *testing.T) *application {
+	return &application{
+		logger: slog.New(slog.NewTextHandler(io.Discard, nil)),
+	}
+}
+
+// Define a custom testServer type which embeds a httptest.Server instance.
+type testServer struct {
+	*httptest.Server
+}
+
+// Create a newTestServer helper which initalizes and returns a new instance
+// of our custom testServer type.
+func NewTestServer(t *testing.T, h http.Handler) *testServer {
+	ts := httptest.NewTLSServer(h)
+	return &testServer{ts}
+}
+
+// Implement a get() method on our custom testServer type. This makes a GET
+// request to a given url path using the test server client, and returns the
+// response status code, headers and body.
+func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, string) {
+	rs, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal()
+	}
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+
+	return rs.StatusCode, rs.Header, string(body)
+}
