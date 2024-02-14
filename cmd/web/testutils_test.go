e5ccd03225c9dfcaced6b49f0187d2bbc236394b
Julian Florez <forezjuli08@hotmail.com>
2024-02-14 15:01:16 -0500

    fix - error handling & trimming body

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"io"
 	"log/slog"
 	"net/http"
@@ -34,11 +35,16 @@ func NewTestServer(t *testing.T, h http.Handler) *testServer {
 func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, string) {
 	rs, err := ts.Client().Get(ts.URL + urlPath)
 	if err != nil {
-		t.Fatal()
+		t.Fatal(err)
 	}
 
 	defer rs.Body.Close()
 	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body = bytes.TrimSpace(body)
 
 	return rs.StatusCode, rs.Header, string(body)
 }
