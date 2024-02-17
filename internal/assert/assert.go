89ae28bf9446f831eed32e334e887bfad8307ceb
Julian Florez <forezjuli08@hotmail.com>
2024-02-17 12:41:40 -0500

    add - a helper test function
    helper for checking if a substr is inside another string

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -1,6 +1,9 @@
 package assert
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 // generic function, we’ll be able to use it irrespective
 // of what the type of the 'actual' and 'expected' values is
@@ -10,5 +13,12 @@ func Equal[T comparable](t *testing.T, actual, expected T) {
 
 	if actual != expected {
 		t.Errorf("got %v, want %v", actual, expected)
+	}
+}
+
+func StringContains(t *testing.T, actual, expectedSubstring string) {
+	t.Helper()
+	if !strings.Contains(actual, expectedSubstring) {
+		t.Errorf("got %v, want %v", actual, expectedSubstring)
 	}
 }
